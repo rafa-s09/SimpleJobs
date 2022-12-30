a10eaf5e86ff8ede280d652c185bf7083b6845f2
fosouzadev <fo.souza@outlook.com>
2022-12-30 18:37:44 -0300

    GetUntilOrEmpty tests

---
+++ src/SimpleJobs/SimpleJobs.Test/Utility/ExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using SimpleJobs.Utility;
+
+namespace SimpleJobs.Test.Utility
+{
+    public class ExtensionsTest : BaseTest
+    {
+        #region GetUntilOrEmpty
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("bbbbb")]
+        public void GetUntilOrEmpty_TextIsNullOrEmptyOrCharacterNotFound_ReturnStringEmpty(string text)
+        {
+            string result = text.GetUntilOrEmpty('a');
+
+            result.Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void GetUntilOrEmpty_ValidText_ReturnSubString()
+        {
+            string text = Fixture.Create<string>();
+            
+            for (int pos = 0; pos < text.Length; pos++)
+            {
+                char character = text[pos];
+                string expectedText = text[..text.IndexOf(character, StringComparison.Ordinal)];
+                
+                string result = text.GetUntilOrEmpty(text[pos]);
+
+                result.Should().Be(expectedText);
+            }
+        }
+
+        [Test]
+        public void GetUntilOrEmpty_InternalError_ThrowArgumentException()
+        {
+            string text = Fixture.Create<string>();
+
+            string result = text.GetUntilOrEmpty('\u0130');
+
+            result.Should().Be(string.Empty);
+        }
+
+        #endregion
+    }
+}
