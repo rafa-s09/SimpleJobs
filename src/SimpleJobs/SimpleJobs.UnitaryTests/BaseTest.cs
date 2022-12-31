6033d47cd81f84aca76d804b8f6499eff734d813
fosouzadev <fo.souza@outlook.com>
2022-12-31 09:19:38 -0300

    entity framework unitary tests

---
+++ src/SimpleJobs/SimpleJobs.UnitaryTests/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+
+namespace SimpleJobs.UnitaryTests
+{
+    public abstract class BaseTest
+    {
+        protected Fixture Fixture { get; }
+
+        public BaseTest()
+        {
+            Fixture = new Fixture();
+        }
+    }
+}
---
+++ src/SimpleJobs/SimpleJobs.UnitaryTests/Repository/Setup/CourseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleJobs.UnitaryTests.Repository.Setup
+{
+    [Table("Course")]
+    public class CourseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(200),Required]
+        public string? Name { get; set; }
+    }
+}
